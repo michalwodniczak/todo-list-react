03c969291369f9d6b24037935133ab8af883f1da
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-03-11 16:04:38 +0100

    added new Component and added style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Tasks from "./Tasks";
 import Section from "./Section";
 import Buttons from "./Buttons";
 import Header from "./Header";
+import Container from "./Container";
 
 const tasks = [
   { id: 1, content: "przejść na reacta", done: false },
@@ -13,18 +14,20 @@ const hideDoneTasks = false;
 
 function App() {
   return (
-    <>
+    <Container>
       <Header title="List zadań" />
-      <Section
-        title="Dodaj nowe zadanie"
-        body={<Form />}
-      />
-      <Section
-        title="Lista zadań"
-        body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
-        extraBody={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />}
-      />
-    </>
+      <main>
+        <Section
+          title="Dodaj nowe zadanie"
+          body={<Form />}
+        />
+        <Section
+          title="Lista zadań"
+          body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+          extraBody={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+        />
+      </main>
+    </Container>
   );
 }
 
