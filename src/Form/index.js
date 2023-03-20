774b42e41aa126c8f521410cc21eec5e92f4e31f
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-03-20 17:06:27 +0100

    added event.preventDefault to Form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,18 @@
 import "./style.css";
-const Form = () => (
-    <form className="form">
-        <input className="form__input" autofocus name="task" required
-            placeholder="Co jest do zrobienia?" />
-        <button className="form__button">Dodaj zadanie</button>
-    </form>
-)
+const Form = () => {
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+        <form className="form" onSubmit={onFormSubmit}>
+            <input className="form__input" autofocus name="task" required
+                placeholder="Co jest do zrobienia?" />
+            <button className="form__button">Dodaj zadanie</button>
+        </form>
+    )
+}
+
+
 
 export default Form
