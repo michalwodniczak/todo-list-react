40990817c42eff2214d2113584d5b6c4df266e49
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-04-22 12:05:27 +0200

    add ternary operator to toggleTaskDone

--- src/App.js
+++ src/App.js
@@ -20,17 +20,12 @@ function App() {
   const removeTasks = (id) => {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
-
+  
   const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => {
-      if (task.id === id) {
-        return {
-          ...task,
-          done: !task.done
-        }
-      }
-      return task;
-    }));
+    setTasks((tasks) => tasks.map((task) => task.id === id
+      ? { ...task, done: !task.done }
+      : task
+    ));
   };
 
   const setAllDone = () => {
