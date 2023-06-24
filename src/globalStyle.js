aa411eac8c2dbd250a420d1c1a845ee7f6075888
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-06-24 11:00:35 +0200

    add GlobalStyle

---
+++ src/globalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *,
+    ::after,
+    ::before {
+        box-sizing: inherit;
+    }
+
+    body{
+         background-color: #eee;
+        padding:20px;
+        font-family: 'Lato', sans-serif;
+    }
+`
