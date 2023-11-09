448c2b3516ef9b6ba153faad743bd01a3989fc0a
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-11-09 16:51:07 +0100

    added store.js and tasksSlice.js

---
+++ src/features/tasks/tasksSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const taskSlice = createSlice({
+    name: "tasks",
+    initialState: {
+        tasks: [],
+        hideDone: false
+    },
+    reducers: {
+        addTask: ({ tasks }, { payload }) => {
+            tasks.push(payload);
+        },
+        toggleHideDone: state => {
+            state.hideDone = !state.hideDone;
+        },
+    },
+});
+
+export const { addTask, toggleHideDone } = taskSlice.actions;
+export const selectTasks = state => state.tasks;
+export default taskSlice.reducer;
