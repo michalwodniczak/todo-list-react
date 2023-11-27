543d5251c4e93dbd22d57f7fa22d63cfe9be8106
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-11-27 18:37:19 +0100

    corrected import url

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -2,7 +2,7 @@ import { Form } from "./Form";
 import { TasksList } from "./TasksList";
 import { Section } from "../../common/Section";
 import { Buttons } from "./Buttons";
-import { Header } from "../../common/Header";
+import { Header} from "../../common/Header";
 import { Container } from "../../common/Container";
 import { Button } from "./Button";
 
