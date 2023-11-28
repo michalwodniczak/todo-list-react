78c7981477d71703e82d7d94ddf80f9cf424d1fe
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-11-28 16:30:47 +0100

    added React-Router construction

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
 import React from "react";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom/";
 import Tasks from "./features/tasks/Tasks";
+import Author from "./features/author/Author";
+import { Navigation } from "./common/Navigation";
 
-export default () => <Tasks />
+export default () => (
+    <HashRouter>
+        <Navigation />
+        <Switch>
+            <Route path="/zadania">
+                <Tasks />
+            </Route>
+            <Route path="/autor" >
+                <Author />
+            </Route>
+            <Route path="/">
+                <Redirect to="/zadania" />
+            </Route>
+        </Switch>
+    </HashRouter>
+);
