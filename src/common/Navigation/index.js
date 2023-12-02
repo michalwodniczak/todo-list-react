e4f557e65d61532b854ad7a4f5eac44ac9cf2d09
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-12-02 10:11:17 +0100

    refactored code

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,14 +1,10 @@
-
 import { StyledNavLink, NaviList } from "./styled";
 
-
 export const Navigation = () => {
-    return (
         <nav>
             <NaviList>
                 <li><StyledNavLink to="/zadania">Zadania</StyledNavLink></li>
                 <li><StyledNavLink to="/autor">o Autorze</StyledNavLink></li>
             </NaviList>
         </nav>
-    );
 };
